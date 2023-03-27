d7a2a0a32d134c25b8e7710a448fe32d1f78b5d7
weronikakus <235909@edu.p.lodz.pl>
2023-03-27 20:54:11 +0200

    spring security change to rest

--- src/main/java/com/mycompany/MainController.java
+++ src/main/java/com/mycompany/MainController.java
@@ -1,15 +1,25 @@
 package com.mycompany;
 
+import com.mycompany.model.user.User;
+import com.mycompany.model.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Iterator;
+import java.util.List;
+
+@RestController
 public class MainController {
 
-    @RequestMapping("/")
-    public String react() {
-        return "index";
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/users")
+    List<User> getUsers(){
+        return userRepository.findAll();
     }
 
 }
