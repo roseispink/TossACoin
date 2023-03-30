f0b6130a9ac10a7f9de649e4cd00be4678e9c7ce
weronikakus <235909@edu.p.lodz.pl>
2023-03-30 17:12:49 +0200

    separate back and front

--- backend/src/main/java/com/mycompany/MainController.java
+++ backend/src/main/java/com/mycompany/MainController.java
@@ -2,9 +2,11 @@ package com.mycompany;
 
 import com.mycompany.model.user.User;
 import com.mycompany.model.user.UserRepository;
+import com.nimbusds.oauth2.sdk.auth.JWTAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,14 @@ public class MainController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping("/user")
     List<User> getUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/test")
+    String getTest(){
+        return "Hello";
     }
 
 }
